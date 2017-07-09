0bac123d8c3a1b2e0e17155799286a2f3aee681c
anahitH <anahithayrapetyan91@gmail.com>
2017-07-09 17:28:53 +0200

    Update run-rc.sh

--- run-rc.sh
+++ run-rc.sh
@@ -7,7 +7,7 @@ bitcode=$1
 pure_functions_config=$2
 con_level=$3
 
-opt -load $RC_LIB/libresult-checking.so $bitcode -insert-guards -sensitive-functions $pure_functions_config -connectivity-level $con_level -o protected.bc
+opt-3.9 -load $RC_LIB/libresult-checking.so $bitcode -insert-guards -sensitive-functions $pure_functions_config -connectivity-level $con_level -o protected.bc
 
-llvm-link protected.bc $RC_RTILB_PATH/response_lib.bc -o protected.bc
+llvm-link-3.9 protected.bc $RC_RTILB_PATH/response_lib.bc -o protected.bc
 
